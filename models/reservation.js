8fca6e7b598d012fa7fa204b922d1dc1f3071673
Vanessa Bragdon <vrbragdon@gmail.com>
2022-11-29 14:38:12 -0600

    save reservation

--- models/reservation.js
+++ models/reservation.js
@@ -8,7 +8,7 @@ const db = require("../db");
 /** A reservation for a party */
 
 class Reservation {
-  constructor({id, customerId, numGuests, startAt, notes}) {
+  constructor({ id, customerId, numGuests, startAt, notes }) {
     this.id = id;
     this.customerId = customerId;
     this.numGuests = numGuests;
@@ -26,17 +26,36 @@ class Reservation {
 
   static async getReservationsForCustomer(customerId) {
     const results = await db.query(
-          `SELECT id, 
+      `SELECT id, 
            customer_id AS "customerId", 
            num_guests AS "numGuests", 
            start_at AS "startAt", 
            notes AS "notes"
          FROM reservations 
          WHERE customer_id = $1`,
-        [customerId]
+      [customerId]
     );
 
     return results.rows.map(row => new Reservation(row));
+  }
+
+  /** save reservation */
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id, start_at, num_guests, notes) 
+        VALUES ($1, $2, $3, $4)
+        RETURNING customer_id, start_at, num_guests, notes`,
+        [this.customerId, this.startAt, this.numGuests, this.notes]
+      )
+      this.id = result.rows[0].id
+    } else {
+      await db.query(
+        `UPDATE reservations SET start_at=$1, num_guests=$2, notes=$3
+        WHERE id=$4`
+        [this.startAt, this.numGuests, this.notes, this.id]
+      )
+    }
   }
 }
 
